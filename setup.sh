d15a62f8f5a04fe01f91ebc0b75bc059b9dcbcfd
John Meneghini <jmeneghi@redhat.com>
2023-02-16 01:52:44 -0500

    setup: add missing packages for rpm builds
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>

--- setup.sh
+++ setup.sh
@@ -183,7 +183,7 @@ popd
 install_pkgs() {
 if [ -f /etc/redhat-release ]; then
 	sudo dnf groupinstall -y "Development tools"
-    sudo dnf install -y --skip-broken asciidoc audit-libs-devel binutils-devel elfutils-devel java-devel kabi-dw libcap-devel libcap-ng-devel libmnl-devel llvm ncurses-devel newt-devel nss-tools numactl-devel pciutils-devel perl perl-generators pesign python3-devel python3-docutils xmlto rpm-build yum-utils sg3_utils dwarves libbabeltrace-devel libbpf-devel openssl-devel net-tools wget bison acpica-tools binutils gcc gcc-c++ git meson cmake dbus-devel libuuid libuuid-devel libuuid-debuginfo json-c-devel json-c-debuginfo json-c json-c-doc libhugetlbfs libhugetlbfs-devel libhugetlbfs-lib clang openssl
+    sudo dnf install -y --skip-broken asciidoc audit-libs-devel binutils-devel elfutils-devel java-devel kabi-dw libcap-devel libcap-ng-devel libmnl-devel llvm ncurses-devel newt-devel nss-tools numactl-devel pciutils-devel perl perl-generators pesign python3-devel python3-docutils xmlto rpm-build yum-utils sg3_utils dwarves libbabeltrace-devel libbpf-devel openssl-devel net-tools wget bison acpica-tools binutils gcc gcc-c++ git meson cmake dbus-devel libuuid libuuid-devel libuuid-debuginfo json-c-devel json-c-debuginfo json-c json-c-doc libhugetlbfs libhugetlbfs-devel libhugetlbfs-lib clang openssl kmod-devel systemd-devel
 fi
 }
 
