8468f5f3acbeda9c1cfe2b916da2c65b389209aa
John Meneghini <jmeneghi@redhat.com>
2023-02-17 15:34:52 -0500

    libnvme: create the rpm library
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>

--- libnvme_rpm/build_rpm.sh
+++ libnvme_rpm/build_rpm.sh
@@ -2,25 +2,67 @@
 # SPDX-License-Identifier: GPL-3.0+
 # Copyright (C) 2023 John Meneghini <jmeneghi@redhat.com> All rights reserved.
 
+TOP=`git rev-parse --show-toplevel`
+. $TOP/rpm_lib.sh
+
 VERSION=1.3
 RELEASE=1
+MODE=srpm
+
+if [ $# -gt 0 ]; then 
+  MODE=$1
+fi
+
+build_dist() {
+	rm -f libnvme-${VERSION}.tar.gz
+	pushd libnvme 
+	make purge
+	make
+	git archive --format=tar HEAD > libnvme-${VERSION}.tar
+	mv libnvme-${VERSION}.tar ../
+	popd
+	tar rf libnvme-${VERSION}.tar libnvme.spec
+	gzip -f -9 libnvme-${VERSION}.tar
+}
+
+prep_rpm() {
+	rm -rf rmpbuild
+	mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+}
+
+build_rpm () {
+	rpmbuild -ta --define "_topdir `pwd`/rpmbuild" -v  libnvme-${VERSION}.tar.gz
+}
+
+build_srpm () {
+	rpmbuild -ts --define "_topdir `pwd`/rpmbuild" -v  libnvme-${VERSION}.tar.gz
+}
+
+pushd $TOP/libnvme_rpm 
+
+case "${MODE}" in
+           srpm)
+             build_dist
+			 prep_rpm
+			 build_srpm
+           ;;
+           rpm)
+             build_dist
+			 prep_rpm
+			 build_rpm
+           ;;
+           copr)
+             build_dist
+			 prep_rpm
+			 build_srpm
+           ;;
+           *)
+           echo " Invalid argument: $MODE" >&2
+		   display_rpm_help
+           popd
+           exit 1
+           ;;
+esac
 
 rm -f libnvme-${VERSION}.tar.gz
-pushd libnvme 
-make purge
-make
-git archive --format=tar HEAD > libnvme-${VERSION}.tar
-mv libnvme-${VERSION}.tar ../
 popd
-
-tar rf libnvme-${VERSION}.tar libnvme.spec
-gzip -f -9 libnvme-${VERSION}.tar
-
-# 
-# https://stackoverflow.com/questions/23965958/how-to-change-rpmbuild-default-directory-form-root-rpmbuild-directory-to-other
-#
-
-rm -rf rmpbuild
-mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-rpmbuild -ta --define "_topdir `pwd`/rpmbuild" -v  libnvme-${VERSION}.tar.gz
-
---
+++ rpm_lib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0+
+# Copyright (C) 2023 John Meneghini <jmeneghi@redhat.com> All rights reserved.
+
+display_rpm_help() {
+  echo " Usage: $0 [ srpm | rpm | copr ]"
+  echo " "
+  echo "  srpm - creates a source rpm in $PWD/rpmbulid"
+  echo "  rpm  - creates a full rpm in $PWD/rpmbulid"
+  echo "  copr - create a source rpm and upload it to the copr dist-git repository"
+  echo ""
+}
+
