fe47769c1adc8ea9bfd39a357969e879fc9a4108
John Meneghini <jmeneghi@redhat.com>
2023-03-09 15:30:48 -0500

    setup: make nbft config programmable
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>

--- host-vm/create_efidisk.sh
+++ host-vm/create_efidisk.sh
@@ -19,6 +19,16 @@ if [ ! -f eficonfig/NvmeOfCli.efi ]; then
 	exit 1
 fi
 
+rm -f eficonfig/config
+cp -v eficonfig/config.in eficonfig/config
+
+sed -i "s/HOSTNQN/$HOSTNQN/" eficonfig/config
+sed -i "s/HOST_MAC2/$HOST_MAC2/" eficonfig/config
+sed -i "s/HOST_IP2/$HOST_IP2/" eficonfig/config
+sed -i "s/HOSTGW_IP2/$HOSTGW_IP2/" eficonfig/config
+sed -i "s/TARGET_IP2/$TARGET_IP2/" eficonfig/config
+sed -i "s/SUBNQN/$SUBNQN/" eficonfig/config
+
 sudo losetup -D loop1
 sudo losetup -P loop1 $PWD/efidisk
 sudo mkfs.vfat /dev/loop1p1
@@ -28,7 +38,9 @@ mkdir -p efi
 sudo mount -t vfat -o loop,offset=1048576 $PWD/efidisk $PWD/efi
 df efi
 sudo tar xzvf efi.tgz
-sudo cp -v $PWD/eficonfig/* $PWD/efi/EFI/BOOT
+sudo cp -v $PWD/eficonfig/config $PWD/efi/EFI/BOOT
+sudo cp -v $PWD/eficonfig/startup.nsh $PWD/efi/EFI/BOOT
+sudo cp -v $PWD/eficonfig/NvmeOfCli.efi $PWD/efi/EFI/BOOT
 sudo umount $PWD/efi
 rmdir efi
 
