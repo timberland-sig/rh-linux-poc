47f69b74bcb28eeb3223995670f647badd186b9e
Michal Rábek <mrabek@redhat.com>
2025-11-21 16:31:00 +0100

    target-vm: bundle the network initialization setup in a systemd service
    Signed-off-by: Michal Rábek <mrabek@redhat.com>

--- vm-lib/netsetup.sh
+++ vm-lib/netsetup.sh
@@ -74,9 +74,17 @@ if ! [ -f .net ] ; then
     case "$VMNAME" in
         target-vm)
             make --makefile="$PWD/Makefile" .build/tcp.json
-            scp -i $DIR/../.ssh/id_ecdsa -o StrictHostKeyChecking=no .build/{tcp.json,hosts.txt} $PWD/start-nvme-target.sh $DIR/../vm-lib/remote-netsetup.sh scp://${SSH_TARGET}
-            ssh -t -i $DIR/../.ssh/id_ecdsa ssh://${SSH_TARGET} "\
-                . remote-netsetup.sh $TARGET_MAC2 $TARGET_MAC3 \"$TARGET_CIDR2\" \"$TARGET_CIDR3\" && . start-nvme-target.sh"
+            scp -i $DIR/../.ssh/id_ecdsa -o StrictHostKeyChecking=no .build/{tcp.json,hosts.txt} $PWD/start-nvme-target.service $PWD/start-nvme-target.sh $DIR/../vm-lib/remote-netsetup.sh scp://${SSH_TARGET}
+            ssh -t -i $DIR/../.ssh/id_ecdsa ssh://${SSH_TARGET} "
+set -e
+. remote-netsetup.sh $TARGET_MAC2 $TARGET_MAC3 \"$TARGET_CIDR2\" \"$TARGET_CIDR3\"
+cp start-nvme-target.sh /usr/local/bin
+cp start-nvme-target.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable --now start-nvme-target.service
+systemctl status start-nvme-target.service
+nvmetcli restore /root/tcp.json
+dmesg | grep nvmet"
         ;;
         host-vm)
             scp -i $DIR/../.ssh/id_ecdsa -o StrictHostKeyChecking=no .build/hosts.txt $DIR/../vm-lib/remote-netsetup.sh scp://${SSH_TARGET}
