30b6652d856aad457148cb15456f92a6ce7ed7bb
John Meneghini <jmeneghi@redhat.com>
2026-09-21 12:53:04 -0400

    target-vm: use QEMU_NETDEV in numa scripts
    Source defaults.sh and replace hardcoded e1000/virtio-net-pci device
    models with $QEMU_NETDEV for consistency with the main vm.sh scripts.
    
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>

--- target-vm/start_numa.sh
+++ target-vm/start_numa.sh
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: GPL-3.0+
 # Copyright (C) 2025 Michal Rábek <mrabek@redhat.com> All rights reserved.
 
+source "$(dirname "$0")/../defaults.sh"
+
 /usr/bin/qemu-system-x86_64 -name target-vm -M q35 -accel kvm -bios OVMF-pure-efi.fd -cpu host,migratable=on \
 	-m 16G -smp cpus=16 \
 	-object memory-backend-ram,size=4G,id=m0 \
@@ -20,11 +22,11 @@
     -numa dist,src=1,dst=3,val=20 \
 	-boot menu=on -vnc :1 -uuid a53caec2-eb2d-4bca-819e-f2bbfb10e1fa \
 	-netdev user,id=net0,net=10.0.2.15/24,hostfwd=tcp::5556-:22 \
-	-device e1000,netdev=net0 \
+	-device $QEMU_NETDEV,netdev=net0 \
 	-netdev bridge,br=$BRIDGE1_NAME,id=net1,helper=/usr/libexec/qemu-bridge-helper \
-	-device virtio-net-pci,netdev=net1,mac=EA:EB:D3:56:89:56 \
+	-device $QEMU_NETDEV,netdev=net1,mac=EA:EB:D3:56:89:56 \
 	-netdev bridge,br=$BRIDGE2_NAME,id=net2,helper=/usr/libexec/qemu-bridge-helper \
-	-device virtio-net-pci,netdev=net2,mac=EA:EB:D3:57:89:57 \
+	-device $QEMU_NETDEV,netdev=net2,mac=EA:EB:D3:57:89:57 \
 	-device nvme,drive=NVME1,max_ioqpairs=4,physical_block_size=4096,logical_block_size=4096,use-intel-id=on,serial=7E1250DCD4B0A268,bootindex=1 \
 	-drive file=/home/test/rh-linux-poc/target-vm/disks/boot.qcow2,if=none,id=NVME1 \
 	-device nvme,drive=NVME2,max_ioqpairs=4,physical_block_size=4096,logical_block_size=4096,use-intel-id=on,serial=97A28D5D3ECDE7F2 \
